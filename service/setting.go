fa80e6719f03e6eac1659fc97a4f9c2dab86f93d
Dean <huangdongqiang@youibot.com>
2026-09-24 13:23:30 +0800

    fix(setting): configure upstream DNS servers and prefer_ipv4 in defaultConfig

--- service/setting.go
+++ service/setting.go
@@ -22,12 +22,24 @@ var defaultConfig = `{
     "level": "info"
   },
   "dns": {
-    "servers": [],
-    "rules": []
+    "servers": [
+      {
+        "tag": "dns-google",
+        "address": "8.8.8.8",
+        "detour": "direct"
+      },
+      {
+        "tag": "dns-cloudflare",
+        "address": "1.1.1.1",
+        "detour": "direct"
+      }
+    ],
+    "strategy": "prefer_ipv4"
   },
   "route": {
+    "auto_detect_interface": true,
     "rules": [
-		  {
+      {
         "action": "sniff"
       },
       {
